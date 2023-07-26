dd28f69606b39f0e1d8e08e18b795153139a85aa
Mann-tech13 <mannshah.ict19@gmail.com>
2023-07-26 17:44:16 +0530

    route bug

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { login, register } = require("../service/userImpl");
 
-router.get("/login", login);
+router.post("/login", login);
 router.post("/register", register);
 
 module.exports = router;
